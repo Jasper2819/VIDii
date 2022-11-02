722da9044500c2687df87d81f839d513708af150
Jasper2819 <jasperriepe@gmail.com>
2022-11-02 15:39:06 +0100

    Changes to HTML, CSS and Java

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,7 @@
 // JavaScript Document
-console.log("Howdy!");
+var buttonSaber = document.querySelector("ol li:first-of-type article button");
+var saber = document.querySelector(".blade")
+
+buttonSaber.addEventListener("click", function() {
+    saber.classList.toggle("open");
+})
